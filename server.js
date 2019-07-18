68c7174830a736603708ac24b8fac460378dc1eb
Lucas Rocha <rocha.lucas20@gmail.com>
2019-07-18 15:38:23 -0300

    Authors part 1

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -15,6 +17,7 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
@@ -23,5 +26,6 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
